5897b736c9af8b35789fb0c6a2822ffca7ae5c6d
capital-eithinzarzaw <60596990+capital-eithinzarzaw@users.noreply.github.com>
2021-11-15 14:28:03 +0630

    etz

---
+++ AppOnDRMS/Models/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppOnDRMS.Models
+{
+    public class BaseModel
+    {
+        public string InsertedBy { get; set; }
+        public string InsertedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public string UpdatedDate { get; set; }
+    }
+}
