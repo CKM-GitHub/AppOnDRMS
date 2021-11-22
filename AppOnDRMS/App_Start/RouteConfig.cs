0509997714188395d639246f00d462dd995bff56
Than Zaw Aung <capital.thanzawaung@gmail.com>
2021-11-22 10:59:15 +0630

    tza

--- AppOnDRMS/App_Start/RouteConfig.cs
+++ AppOnDRMS/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AppOnDRMS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "User", action = "UserLogin", id = UrlParameter.Optional }
             );
         }
     }
