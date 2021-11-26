fe5a0a390a476023c6df44b35b71f5e308baf033
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2021-11-26 17:28:41 +0630

    pnz

--- AppOnDRMS/Controllers/DailyReportController.cs
+++ AppOnDRMS/Controllers/DailyReportController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DRMS_Models;
+using User_BL;
 
 namespace AppOnDRMS.Controllers
 {
@@ -15,7 +16,11 @@ namespace AppOnDRMS.Controllers
             HttpCookie cookie = HttpContext.Request.Cookies.Get("Other_Member_ID");
             if (cookie != null)
             {
+                UserBL user_bl = new UserBL();
                 ulmodel.member_id = cookie.Value;
+                CompanyModel com_Model = user_bl.GetCompanyName();
+                ulmodel.company_name = com_Model.company_name;
+                ulmodel.company_id = com_Model.company_id;
                 return View(ulmodel);
             }
             else
