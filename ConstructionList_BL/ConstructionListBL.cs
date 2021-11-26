17b488a05c4bb5661e539d39073afecd9ee5fa56
Than Zaw Aung <capital.thanzawaung@gmail.com>
2021-11-26 15:30:41 +0630

    tza

--- ConstructionList_BL/ConstructionListBL.cs
+++ ConstructionList_BL/ConstructionListBL.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CKM_CommonFunction;
+using CKM_DataLayer;
 
 namespace ConstructionList_BL
 {
     public class ConstructionListBL
     {
+        CKMDL dl = new CKMDL();
+        FileFunction ff;
+        public string GetPrjData()
+        {
+            return dl.SelectJson("GetProjectName", ff.GetConnectionWithDefaultPath("AppOnDRMS"));
+        }
     }
 }
